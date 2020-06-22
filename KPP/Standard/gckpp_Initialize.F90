70c9495656223e2bf6b29673ea544568565b0ad0
Colin Pike-Thackray <thackray@seas.harvard.edu>
2020-06-22 15:02:56 -0400

    HgP case in KPP

--- KPP/Standard/gckpp_Initialize.F90
+++ KPP/Standard/gckpp_Initialize.F90
@@ -13,7 +13,7 @@
 !        R. Sander, Max-Planck Institute for Chemistry, Mainz, Germany
 ! 
 ! File                 : gckpp_Initialize.f90
-! Time                 : Mon Jun 22 13:19:52 2020
+! Time                 : Mon Jun 22 14:20:07 2020
 ! Working directory    : /n/home02/thackray/GC_working/KPP/Standard
 ! Equation file        : gckpp.kpp
 ! Output root filename : gckpp
